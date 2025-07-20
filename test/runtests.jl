4decb8444520c5f96085181c68dfb75cbc30b1cd
Matthew Meeker <meeker.matthew@pm.me>
2025-07-20 14:32:05 -0400

    A bit more

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,19 @@ using Test
 
 @testset "BlackHoles" begin
 
+
+    @testset "pe_2_EL" begin
+
+        output = BlackHoles.pe_2_EL(1.0, 2.0)
+
+        @test output[1] == 1.0
+        @test output[2] == 1.5
+        @test output[3] == 1.0
+
+
+
+    end
+
     @test BlackHoles.addition(1, 2) == 3
 
     @test BlackHoles.addition(3, 4) == 7
