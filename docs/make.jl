f152418343841676c27fb2bdcfbc48cb558339d4
Ref <mathrefath@gmail.com>
2025-10-13 02:16:33 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,10 @@ makedocs(
     sitename = "BlackHoles.jl",
     modules  = [BlackHoles],
     format   = Documenter.HTML(; prettyurls = get(ENV, "CI", "false") == "true"),
-    pages = [
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
 deploydocs(
     repo      = "github.com/RefBari/BlackHoles.jl.git",
-    devbranch = "main",          # change to "master" if that’s your default
+    devbranch = "main",
 )
