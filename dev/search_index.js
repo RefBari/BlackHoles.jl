ec7c51585c47c595446cd2325c066ac8b404e7c8
Documenter.jl <documenter@juliadocs.github.io>
2025-10-13 06:41:09 +0000

    build based on 86a2201

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BlackHoles.jl","page":"Home","title":"BlackHoles.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Minimal docs site. Edit me!","category":"page"},{"location":"#Quick-start","page":"Home","title":"Quick start","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"```julia pkg> add https://github.com/RefBari/BlackHoles.jl using BlackHoles","category":"page"}]
+}
