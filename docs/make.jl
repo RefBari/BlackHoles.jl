514d86d7f917af468f4acafcba150fa862ac571d
Ref <mathrefath@gmail.com>
2025-10-16 20:56:37 -0400

    Expand documentation structure in make.jl
    Added new documentation pages for Conservative and Dissipative Dynamics, including topics on Schwarzschild Metric and GENERIC Formalism.

--- docs/make.jl
+++ docs/make.jl
@@ -5,10 +5,26 @@ makedocs(
     sitename  = "BlackHoles.jl",
     modules   = [BlackHoles],
     format    = Documenter.HTML(; prettyurls = get(ENV, "CI", "false") == "true"),
-    pages     = ["Home" => "index.md"],
-    checkdocs = :none,                 # don’t enforce docstrings right now
-    warnonly  = [:missing_docs],       # treat missing docs as warnings, not errors
+    pages = [
+        "Home" => "index.md",
+        "Conservative Dynamics" => [
+            "Physics of Schwarzschild Metric"    => "guide/schwarzschild-metric.md",
+            "Simulating Orbits in Schwarzschild Metric"  => "guide/orbits.md",
+            "Quadrupole Approximation: Orbits to Gravitational Wave"        => "guide/waveforms.md",
+            "Adding Higher Order Modes"        => "guide/higher_modes.md",
+            "Adding a Neural Network: Waves to Orbits"        => "guide/inverse_problem.md"
+        ],
+        "Dissipative Dynamics" => [
+            "Theory: GENERIC Formalism"    => "guide/GENERIC.md",
+            "Simulating Orbits with GENERIC"  => "guide/dissipative_orbits.md",
+            "Dissipative Case: Waves to Orbits" => "guide/full_inverse_problem.md"
+        ],
+        "API"  => "api.md",
+    ],
+    checkdocs = :none,
+    warnonly  = [:missing_docs],
 )
+
 
 deploydocs(
     repo      = "github.com/RefBari/BlackHoles.jl.git",
